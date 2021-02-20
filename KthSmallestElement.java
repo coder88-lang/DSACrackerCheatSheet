af1e51ac89c30b4c6ad18b4d0cc11ed63d63f18d
Lavisha Soni <67314839+coder88-lang@users.noreply.github.com>
2021-02-20 23:02:22 +0530

    Add files via upload

---
+++ KthSmallestElement.java
@@ -0,0 +1,14 @@
+/*Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.*/
+
+//https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
+
+public class KthSmallestElement {
+
+}
