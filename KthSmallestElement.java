b3accca5c6ff50575a2b17c67f05c0583580371d
Lavisha Soni <67314839+coder88-lang@users.noreply.github.com>
2021-02-24 11:55:31 +0530

    Add files via upload

--- KthSmallestElement.java
+++ KthSmallestElement.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 /*Input:
 N = 6
 arr[] = 7 10 4 3 20 15
@@ -10,5 +13,49 @@ array is 7.*/
 //https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
 
 public class KthSmallestElement {
-
+	public static void main(String[] args) {
+		int arr[] = {11, 3, 2, 1, 15, 5, 4, 45, 88, 96, 50, 4};
+		int k = 3;
+		System.out.println("Given array...");
+		print(arr, arr.length);
+		System.out.println();
+		kthLargestElement(arr, arr.length, k);
+		kthSmallestElement(arr, arr.length, k);
+	}
+	public static void kthLargestElement(int a[], int n, int k) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i = 0; i<n; i++) {
+			pq.add(a[i]);
+			if (pq.size() > k) {
+				pq.poll();
+			}
+			pq.peek();
+		}
+		//return the first kth element...
+		while (pq.size() > 0) {
+			System.out.println(pq.poll());
+		}
+	
+	}
+	public static void kthSmallestElement(int a[], int n, int k) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		for (int i = 0; i<n; i++) {
+			pq.add(a[i]);
+			if (pq.size() > k) {
+				pq.poll();
+			}
+			pq.peek();
+		}
+		//return the first kth element...
+		while (pq.size() > 0) {
+			System.out.println(pq.poll());
+		}
+	
+	}
+	public static void print(int a[], int n) {
+		for (int i = 0; i<n; i++) {
+			System.out.print(a[i] + " ");
+		}
+		
+	}
 }
